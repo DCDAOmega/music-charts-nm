a80bd541ed315dee55df51a8ac0c466e31a32a17
David <david.angelakis@gmail.com>
2018-07-16 15:56:41 -0500

    Readme verbiage change and removed unused import

--- src/app/tracks/tracks.component.ts
+++ src/app/tracks/tracks.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnChanges, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Track} from '../track.model';
 import {TracksService} from '../tracks.service';
 
